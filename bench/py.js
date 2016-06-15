9fd7fa87edb0631522fdc89bebbf9d752b4770df
Pavel Medvedev <pmedvedev@gmail.com>
2016-06-15 12:46:43 +0300

    benchmarks: use process.hrtime() to measure time
    Use process.hrtime() to measure elapsed time in benchmarks more
    accurately.

--- bench/py.js
+++ bench/py.js
@@ -16,9 +16,10 @@ const obj = JSON.stringify(deep(5));
 
 const COUNT = 1e3;
 
-const start = +new Date;
+const start = process.hrtime();
 for (let i = 0; i < COUNT; i++)
   py.parseJSON(obj);
-const end = +new Date;
+const end = process.hrtime(start);
+const elapsed_seconds = (end[0] * 10e9 + end[1]) / 10e9;
 
-console.log('%d parseJSON ops/sec', COUNT / (end - start) * 1000);
+console.log('%d parseJSON ops/sec', (COUNT / elapsed_seconds).toFixed(3));
