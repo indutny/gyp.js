3a0142cdf4a7db55dd29ecb2ac2edb1063c960fd
Fedor Indutny <fedor@indutny.com>
2016-07-01 16:52:19 -0400

    test: use `ninja.js`

--- test/gyppies-test.js
+++ test/gyppies-test.js
@@ -6,15 +6,15 @@ const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 const rimraf = require('rimraf');
+const ninja = require('ninja.js');
 const spawnSync = require('child_process').spawnSync;
 
 const gyp = path.join(__dirname, '..', 'bin', 'gyp');
-const ninja = process.env.NINJA || 'ninja';
 const gyppiesDir = path.join(__dirname, 'gyppies');
 const tests = fs.readdirSync(gyppiesDir);
 
 function build(name) {
-  it(`should build ${name}`, function() {
+  it(`should build ${name}`, function(cb) {
     this.timeout(15000);
 
     const folder = path.join(gyppiesDir, name);
@@ -30,31 +30,34 @@ function build(name) {
       throw p.error;
     assert.equal(p.status, 0, `cd ${name} && gyp failed`);
 
-    p = spawnSync(ninja, [ '-C', path.join('out', 'Default') ], spawnOpts);
-    if (p.status !== 0 && p.stdout)
-      console.error(p.stdout.toString());
-    if (p.error)
-      throw p.error;
-    assert.equal(p.status, 0, `ninja ${name}`);
+    const ninjaArgs = [
+      'node', 'ninja', '-C', path.join(folder, 'out', 'Default') ];
+    ninja.cli.run(ninjaArgs, {
+    }, (err) => {
+      if (err)
+        return cb(err);
 
-    // Compiled test
-    let test = path.join(folder, 'out', 'Default', 'test');
-    if (fs.existsSync(test)) {
-      p = spawnSync(test, [], {
-        stdio: stdio,
-        cwd: path.join(folder, 'out', 'Default')
-      });
-      if (p.status !== 0 && p.stdout)
-        console.error(p.stdout.toString());
-      if (p.error)
-        throw p.error;
-      assert.equal(p.status, 0, `test ${name}`);
-    }
+      // Compiled test
+      let test = path.join(folder, 'out', 'Default', 'test');
+      if (fs.existsSync(test)) {
+        p = spawnSync(test, [], {
+          stdio: stdio,
+          cwd: path.join(folder, 'out', 'Default')
+        });
+        if (p.status !== 0 && p.stdout)
+          console.error(p.stdout.toString());
+        if (p.error)
+          return cb(p.error);
+        assert.equal(p.status, 0, `test ${name}`);
+      }
 
-    // JavaScript test
-    test = path.join(folder, 'test.js');
-    if (fs.existsSync(test))
-      require(test)(path.join(folder, 'out', 'Default'));
+      // JavaScript test
+      test = path.join(folder, 'test.js');
+      if (fs.existsSync(test))
+        require(test)(path.join(folder, 'out', 'Default'));
+
+      cb(null);
+    });
   });
 }
 
