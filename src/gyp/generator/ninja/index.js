e86792c3462bbcc8cbe16b480bd10c4bf9d43303
Fedor Indutny <fedor@indutny.com>
2017-03-21 14:00:21 +0100

    ninja: proper target resolution on linux/win32

--- src/gyp/generator/ninja/index.js
+++ src/gyp/generator/ninja/index.js
@@ -184,6 +184,10 @@ Ninja.prototype.type = function type() {
   return this.targetDict.type;
 };
 
+Ninja.prototype.uniqueOutPath = function uniqueOutPath(out) {
+  return common.path.join(this.objDir, out);
+};
+
 Ninja.prototype.output = function output() {
   let res = [];
 
@@ -225,6 +229,17 @@ Ninja.prototype.output = function output() {
     out = out.slice(3);
 
   out = prefix + out;
+
+  // This is really stupid, but GYP does it and we aim to be compatible with
+  // it to be able to build node
+  const productDirTypes = [ 'executable', 'loadable_module' ];
+  if (this.flavor === 'darwin' && this.toolset === 'target')
+    productDirTypes.push('shared_library', 'static_library');
+  else if (this.flavor === 'win32' && this.toolset === 'target')
+    productDirTypes.push('shared_library');
+
+  if (productDirTypes.indexOf(type) === -1)
+    out = this.uniqueOutPath(out);
 
   if (type !== 'none')
     res.push(out);
